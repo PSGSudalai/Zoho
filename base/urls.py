e6a1809d5da95f692309bc018e8bb162ce0d32ab
PSGSudalai <sudalaimuthug397@gmail.com>
2024-11-08 20:49:14 +0530

    newpage

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('', views.dashboard, name='home'),
+    path('dashboard/', views.dashboard, name='home'),
     path('add/lead/', views.add_lead, name='add-lead'),
     path('lead/list/',views.all_lead,name='lead'),
     path('lead/edit/<int:lead_id>/', views.edit_lead, name='edit-lead'),
